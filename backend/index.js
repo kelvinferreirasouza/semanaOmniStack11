af3861051a7485f9d0eedb8044ba84a50a0428b3
Kelvin Souza <kelvinferreirasouza1@gmail.com>
2020-03-27 19:37:18 -0300

    Adicionado o Nodemon

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,33 @@ const express = require('express');
 
 const app = express();
 
+/**
+ * Rota / Recurso
+ */
+
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ * 
+ */
+
+/**
+ * Tipos de parâmetros:
+ * 
+ * Query: Parâmetros nomeados enviados na rota após "?" utilizado para Filtros, Paginação..
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ */
+
+
+
 app.get('/users', (request, response) => {
+
+
     return response.json({
         evento: 'Semana de Curso',
         aluno: 'Kelvin Ferreira Souza'
