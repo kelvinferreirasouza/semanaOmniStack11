ae5cc384520706e3a35c5636fd8739c350a136eb
Kelvin Souza <kelvinferreirasouza1@gmail.com>
2020-03-30 19:29:47 -0300

    Desenvolvida a page Register e o CSS da mesma

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
 
 import './styles.css';
 
 export default function Register(){
-    return <h1>Register</h1>
+    return (
+        <div className="register-container">
+            <div className="content">
+                <section>
+                    <img src={logoImg} alt="COVID19 Incidents"/>
+
+                    <h1>Cadastro</h1>
+                    <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos de Corona Virus no Brasil.</p>
+
+                    <Link to="/" className="back-link">
+                        <FiArrowLeft size={16} color="#E02041" />
+                        Não tenho cadastro
+                    </Link>
+                </section>
+
+                <form>
+                    <input placeholder="Nome do Hospital" />
+                    <input type="email" placeholder="Email" />
+                    <input placeholder="Contato" />
+
+                    <div className="input-group">
+                        <input placeholder="Cidade" />
+                        <input placeholder="UF" style={{ width: 80 }} />
+                    </div>
+
+                    <button className="button" type="submit">Cadastrar</button>
+                </form>
+            </div>
+        </div>
+    )
 }
