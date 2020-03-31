4bbe488243a9c5b174cbe39387aac9d4795e1ff9
Kelvin Souza <kelvinferreirasouza1@gmail.com>
2020-03-31 20:38:47 -0300

    Criado a integração com o backend e criado o metodo de Register

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,12 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import './styles.css';
 
 export default function Register(){
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [contact, setContact] = useState('');
+    const [city, setCity] = useState('');
+    const [uf, setUf] = useState('');
+
+    const history = useHistory();
+    
+    async function handleRegister(e){
+        // verifica se o formulário está vazio, se sim, não atualiza a página ao clicar no botão
+        e.preventDefault();
+
+        const data = {
+            name,
+            email,
+            contact,
+            city,
+            uf,
+        };
+
+        try {
+            const response = await api.post('hospitals', data);
+            alert(`Seu ID de acesso: ${ response.data.id } `);
+            
+            // funcao que envia o usuario para a rota determinada
+            history.push('/');
+        } catch (err) {
+            alert('Erro no cadastro, tente novamente.');
+        }
+    }
+
+
     return (
         <div className="register-container">
             <div className="content">
@@ -22,14 +57,42 @@ export default function Register(){
                     </Link>
                 </section>
 
-                <form>
-                    <input placeholder="Nome do Hospital" />
-                    <input type="email" placeholder="Email" />
-                    <input placeholder="Contato" />
+                <form onSubmit={handleRegister}>
+
+                    <input 
+                        placeholder="Nome do Hospital"
+                        value={name}
+                        onChange={ e => setName(e.target.value) }
+                    />
+
+                    <input 
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={ e => setEmail(e.target.value) }
+                    />
+
+                    <input 
+                        placeholder="Contato"
+                        value={contact}
+                        onChange={ e => setContact(e.target.value) }
+                    />
 
                     <div className="input-group">
-                        <input placeholder="Cidade" />
-                        <input placeholder="UF" style={{ width: 80 }} />
+
+                        <input 
+                            placeholder="Cidade"
+                            value={city}
+                            onChange={ e => setCity(e.target.value) }
+                        />
+
+                        <input 
+                            placeholder="UF"
+                            value={uf}
+                            onChange={ e => setUf(e.target.value) }
+                            style={{ width: 80 }}
+                        />
+
                     </div>
 
                     <button className="button" type="submit">Cadastrar</button>
